007baa4303ce21893c4f97d4ea42065fede3b4b2
Martin D. <martindjanov@gmail.com>
2024-11-14 20:38:58 +0200

    One-Dimensional Arrays

---
+++ src/_4_Arrays/_04_MonthTemperatures.java
@@ -0,0 +1,73 @@
+package _4_Arrays;
+
+import java.util.*;
+
+public class _04_MonthTemperatures {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        String month;
+        boolean validMonth; // wrong input "flag"
+
+        do { // wrong input check
+            // Input current month
+            System.out.println("Input a month: ");
+            month = kb.nextLine();
+
+            validMonth = month.equals("January") || month.equals("February") || month.equals("March") ||
+                    month.equals("April") || month.equals("May") || month.equals("June") || month.equals("July") ||
+                        month.equals("August") || month.equals("September") || month.equals("October") ||
+                            month.equals("November") || month.equals("December");
+        }
+        while (!validMonth);
+
+        int monthDays = 0;
+
+        switch (month) {
+            case "January": case "March": case "May": case "July":
+                case "August": case "October": case "December":
+
+                    monthDays = 31;
+                break;
+
+            case "February": case "April": case "June":
+                case "September": case "November":
+
+                    monthDays = 30;
+                break;
+        }
+        // variable for specified temperature
+        double preferredTemp;
+
+        do {
+            System.out.println("Input preferred average temperature per a day: ");
+            // Variable is assigned а value compared to array's input values (by weather forecast's stats)
+            preferredTemp = kb.nextDouble();
+        }
+        while ((preferredTemp < -40) || (preferredTemp > 55));
+
+        double[] dailyTemp = new double[monthDays]; // (forecast's stats)
+
+        // counter (for all days with a temperature == 'preferredTemp')
+        int daysPreferredTemp = 0;
+
+        // assigning values for each day (of month)
+        for (int i = 0; i < monthDays; i++) {
+            do {
+                System.out.println("Input the average temperature for today " + (i + 1) + " " + month);
+                dailyTemp[i] = kb.nextDouble();
+            } // varying-normal range of temperatures check
+            while ((dailyTemp[i] < -40 || dailyTemp[i] > 55));
+
+            if (dailyTemp[i] == preferredTemp) {
+                daysPreferredTemp++;
+            }
+        }
+
+        System.out.println("In " + month + ", " + daysPreferredTemp +
+                " days had an average temperature of " + preferredTemp + " degrees Celsius");
+
+        kb.close();
+    }
+}
---
+++ src/_4_Arrays/_05_GradeScore.java
@@ -0,0 +1,38 @@
+package _4_Arrays;
+
+import java.util.*;
+
+public class _05_GradeScore {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        int students;
+
+        do {
+            System.out.println("Students in the class: ");
+            students = kb.nextInt();
+        }
+        while (students > 32);
+
+        double[] averageGrade = new double[students];
+
+        double max = averageGrade[0];
+
+        for (int i = 0; i < students; i++) {
+            do {
+                System.out.println("Average grade of Student №" + (i + 1) + ":");
+                averageGrade[i] = kb.nextDouble();
+            }
+            while ((averageGrade[i] < 2.00) || (averageGrade[i] > 6.00));
+
+            if (averageGrade[i] > max) {
+                max = averageGrade[i];
+            }
+        }
+
+        System.out.println("The highest grade is " + max);
+
+        kb.close();
+    }
+}
