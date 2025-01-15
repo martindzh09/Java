5a129a1c879b3dacaa11e2f701e70f49bfbd3729
Martin D. <martindjanov@gmail.com>
2025-01-15 15:47:02 +0200

    Type String

--- src/_5_VariablesTypes/_04_Words.java
+++ src/_5_VariablesTypes/_04_Words.java
@@ -1,50 +1,48 @@
 package _5_VariablesTypes;
 
-import java.util.*; import java.io.IOException;
+import java.util.*;
 
 public class _04_Words {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Scanner kb = new Scanner(System.in);
 
         System.out.println("Input a word: ");
         String word = kb.nextLine();
 
-        System.out.println("Input another word: ");
+        System.out.println("\n" + "Input another word: ");
         String word2 = kb.nextLine();
 
+        // a message to visualize the words typed in above
         System.out.println("\n" + "Words: " + word + ", " + word2);
 
+        // list of two options is printed
         System.out.println("\n" + "Choose an option for words.");
         System.out.println("A. To lowercase letters" + "\n" + "B. To uppercase letters");
 
+        String option; // one of the letters 'A' or 'B', of the options above
 
-        char option;
+        do { // different/wrong option input check
+            System.out.print("\n"); option = kb.nextLine();
 
-        do {
-            option = (char) System.in.read();
-
-            if ((option != 'A') && (option != 'B')) {
+            if (!option.equals("A") && !option.equals("B")) {
                 System.out.println("Non-available option.");
-                option = (char) System.in.read();
             }
-        }
-        while ((option != 'A') && (option != 'B'));
+        } while (!option.equals("A") && !option.equals("B"));
 
         switch (option) {
-            case 'A':
-                // to Lowercase letters
+            case "A": // to Lowercase letters
                 word = word.toLowerCase();
                 word2 = word2.toLowerCase();
                 break;
 
-            case 'B':
-                // to Uppercase letters
+            case "B": // to Uppercase letters
                 word = word.toUpperCase();
                 word2 = word2.toUpperCase();
                 break;
         }
 
+        // printing transformed words
         System.out.println("\n" + "Transformed words: " + word + ", " + word2);
 
         kb.close();
