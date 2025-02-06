f7022e28dc161fa51986ca6b792c279c8cbab359
Martin D. <martindjanov@gmail.com>
2025-02-06 18:48:43 +0200

    Methods

--- src/_6_Methods/_03_ReturnArea_Part2.java
+++ src/_6_Methods/_03_ReturnArea_Part2.java
@@ -4,41 +4,41 @@ import java.util.*;
 
 public class _03_ReturnArea_Part2 {
 
-	// Method 1 - area of a rectangle
-	public static double area(double a, double b) {
-	
-		return a * b; // returning the result of multiplication
-	}
-	
-	// Method 2 - wrong input check
-	public static double inputCheck() {
-		Scanner scr = new Scanner(System.in);
+    // Method 1 - area of a rectangle
+    public static double area(double a, double b) {
 
-		double num;
-		
-		do { // wrong input check
-			num = scr.nextDouble();
-			
-			if (num <= 0) {
-				System.out.println("Wrong input!");
-				System.out.println("\n" + "Input again: ");
-			}
-		} while (num <= 0);
+        return a * b; // returning the result of multiplication
+    }
 
-		return num; // value is returned
-	}
-	
-	// Main method
-	public static void main(String[] args) {
-		
-		System.out.println("\n" + "Input data for a length 'a': ");
-		double a = inputCheck(); // method 2 is called
-		
-		System.out.println("\n" + "Input data for a width 'b'");
-		double b = inputCheck(); // method 2 is called
-		
-		// calling method 1 - the result
-		System.out.println("\n" + "The area of rectangle with sides " + 
-				a + " cm and " + b + " cm is: " + area(a, b) + " cm2");
-	}
+    // Method 2 - wrong input check
+    public static double inputCheck() {
+        Scanner scr = new Scanner(System.in);
+
+        double num;
+
+        do { // wrong input check
+            num = scr.nextDouble();
+
+            if (num <= 0) {
+                System.out.println("Wrong input!");
+                System.out.println("\n" + "Input again: ");
+            }
+        } while (num <= 0);
+
+        return num; // value is returned
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        System.out.println("\n" + "Input data for a length 'a': ");
+        double a = inputCheck(); // method 2 is called
+
+        System.out.println("\n" + "Input data for a width 'b'");
+        double b = inputCheck(); // method 2 is called
+
+        // calling method 1 - the result
+        System.out.println("\n" + "The area of rectangle with sides " +
+                a + " cm and " + b + " cm is: " + area(a, b) + " cm2");
+    }
 }
